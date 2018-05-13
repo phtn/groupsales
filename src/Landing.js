350069361e4270348e43ae57b0088a979bc7a0c9
phtn458 <phtn458@gmail.com>
2018-05-12 23:50:48 -0400

    handled collapsing Log In button

--- src/Landing.js
+++ src/Landing.js
@@ -4,8 +4,8 @@ export default props => (
   <div>
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <p className="navbar-brand" style={{marginTop: 10}}>Clarion Group Sales</p>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+      <button onClick={props.sign} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="sign-in-alt">{props.user || 'Log In'}</span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarColor01">
@@ -25,7 +25,7 @@ export default props => (
         </ul>
         <form className="form-inline my-2 my-lg-0">
         {/* <input className="form-control mr-sm-2" type="Password" placeholder="password"/> */}
-        <button className="btn btn-secondary my-2 my-sm-0" onClick={props.sign}>{props.user || 'Log in'}</button>
+        <button className="btn btn-secondary my-2 my-sm-0" onClick={props.sign}>{props.user || 'Log In'}</button>
         </form>
       </div>
     </nav>
